e17a2e616e93d3ed08efe2cc7ade42c6f38e34f8
Hariprashad Ravikumar <109697412+Hariprashad-Ravikumar@users.noreply.github.com>
2025-05-18 17:46:18 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder="static", template_folder="../frontend")
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "https://hariprashad-ravikumar.github.io"}})
 @app.route("/")
 def home():
     return render_template("index.html", summary="", log="", forecast="", plot_url=None)
